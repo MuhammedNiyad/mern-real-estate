97a995eb71b04b272b70177e3babf54701d6c032
Muhammed Niyad <niyadpunniyath007@gmail.com>
2023-12-06 10:28:59 -0800

    create a middleware and a errorhandler function

---
+++ api/utils/error.js
@@ -0,0 +1,7 @@
+export const errorHandler = (statusCode, message) => {
+    const error = new Error()
+    console.log(Error());
+    error.statusCode = statusCode;
+    error.message = message;
+    return error;
+}
